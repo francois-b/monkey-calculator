9cd5db54a27540fd4d8af020d2437be84a871131
Francois Bouet <f.bouet@gmail.com>
2017-11-13 19:07:35 +0100

    Use Space Bar to Enable Monkey Mode

--- src/index.jsx
+++ src/index.jsx
@@ -8,12 +8,26 @@ import thunk from 'redux-thunk';
 
 import { ConnectedApp } from './components/calculator.jsx';
 import { appReducer } from './reducers';
+import { monkeyModeActivation, monkeyModeDeactivation, monkeyClickAsync } from './actions';
 
 const store = createStore(
   appReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk),
 );
+
+function handleSpaceBarPress(event) {
+  if (event.code === 'Space') {
+    if (store.getState().calculator.monkeyMode) {
+      store.dispatch(monkeyModeDeactivation());
+    } else {
+      store.dispatch(monkeyModeActivation());
+      store.dispatch(monkeyClickAsync());
+    }
+  }
+}
+
+document.onkeydown = handleSpaceBarPress;
 
 render(
   <Provider store={store}>
